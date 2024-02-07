39a64040bd83144b623160feb802e52d3df6e2b3
silvasebastian990906@gmail.com <silvasebastian990906@gmail.com>
2024-02-07 17:00:42 -0500

    add method formatDate

--- lib/features/global/commun/transversals.dart
+++ lib/features/global/commun/transversals.dart
@@ -8,3 +8,13 @@ Future<File> fileFromAssets(String path) async {
 
   return file;
 }
+
+String formatDate(DateTime date, {order = 1}) {
+  var month = date.month > 9 ? date.month : "0${date.month}";
+  var day = date.day > 9 ? date.day : "0${date.day}";
+  if (order == 2) {
+    return "${date.year}-$month-$day";
+  } else {
+    return "$day-$month-${date.year}";
+  }
+}
